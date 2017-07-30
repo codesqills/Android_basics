2758bd6f6611399e1dd074f11162f25da28544bf
codesqills <kamalshree2784@gmail.com>
2017-07-30 15:31:51 -0400

    ListView usingArrayAdapter

---
+++ ListViewArrayAdapter/app/src/main/java/org/codesqills/listviewarrayadapter/ListViewActivity.java
@@ -0,0 +1,38 @@
+package org.codesqills.listviewarrayadapter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview);
+
+        /* create the ArrayList */
+        ArrayList<String> Languages=new ArrayList<String>();
+        Languages.add("Android");
+        Languages.add("Java");
+        Languages.add("HTML5");
+        Languages.add("CSS");
+        Languages.add("JavaScript");
+        Languages.add("Php");
+        Languages.add("Jquery");
+        Languages.add("Ruby");
+
+        /* create the ArrayAdapter and add the ArrayList Languages */
+        ArrayAdapter<String> myAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Languages);
+
+        /* get teh reference of the list view from the layout file */
+        ListView mylistview=(ListView)findViewById(R.id.mylistview);
+
+        /* set the Adapter on the ListView */
+        mylistview.setAdapter(myAdapter);
+
+    }
+}
