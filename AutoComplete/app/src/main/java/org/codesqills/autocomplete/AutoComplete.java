0d34c71eadca57072acf1969cd5a344351994940
codesqills <kamalshree2784@gmail.com>
2017-08-06 23:09:24 -0400

    Autocomplete feature

---
+++ AutoComplete/app/src/main/java/org/codesqills/autocomplete/AutoComplete.java
@@ -0,0 +1,29 @@
+package org.codesqills.autocomplete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class AutoComplete extends AppCompatActivity {
+
+    AutoCompleteTextView autoCompleteTextView;
+    String[] countryname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_complete);
+
+        /* Instantiate the object */
+        autoCompleteTextView=(AutoCompleteTextView)findViewById(R.id.autocompletefield);
+
+        /* get the list of countries from the resource file */
+        countryname=getResources().getStringArray(R.array.countries);
+
+        /* create the adapter that reference the resources file */
+        ArrayAdapter<String> myadapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,countryname);
+
+        autoCompleteTextView.setAdapter(myadapter);
+    }
+}
